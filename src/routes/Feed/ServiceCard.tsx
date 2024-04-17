eed04bb8fb58bf83607e3ea009ac5a7eab929e91
박철준 <dizncjfwns@nate.com>
2024-04-17 22:34:33 +0900

    Fix: edit side bar text

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -9,7 +9,7 @@ const ServiceCard: React.FC = () => {
   return (
     <>
       <StyledTitle>
-        <Emoji>🌟</Emoji> Service
+        <Emoji>🌟</Emoji> My Project's Repo
       </StyledTitle>
       <StyledWrapper>
         {CONFIG.projects.map((project, idx) => (

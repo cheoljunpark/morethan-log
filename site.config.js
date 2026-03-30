7af1860c06d7c6bf12e2edc2450868a8564edcd5
DESKTOP-ES1I4RU\user <dkfnak6789@gmail.com>
2026-03-30 11:46:15 +0900

    test commit

--- site.config.js
+++ site.config.js
@@ -85,7 +85,7 @@ const CONFIG = {
     enable: true,
     config: {
       host: "https://cusdis.com",
-      appid: "", // Embed Code -> data-app-id value
+      appid: "", // Embed Code -> data-app-id value 
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
